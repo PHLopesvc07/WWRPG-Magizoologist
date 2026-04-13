8d53c3c05b837eb88fdeffeb3d37463a50e484a6
PHLopesvc07 <lsaithama@gmail.com>
2026-04-12 23:14:36 -0300

    Refractoring the code phase 1

---
+++ js/main.js
@@ -0,0 +1,12 @@
+import { TabController } from './controllers/TabController.js';
+import { CreatureController } from './controllers/CreatureController.js';
+import { SpellController } from './controllers/SpellController.js';
+
+document.addEventListener('DOMContentLoaded', () => {
+    // A Injeção de Dependências ou Instanciação Modular começa aqui.
+    console.log("Inicializando o Ministério da Magia - DRCCM...");
+
+    const tabController = new TabController();
+    const creatureController = new CreatureController();
+    const spellController = new SpellController();
+});
